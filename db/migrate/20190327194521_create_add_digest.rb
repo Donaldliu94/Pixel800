a4014bd0323911d064b8cb14b750e1f8c2bc1b22
DLiu <Donaldliu21194@gmail.com>
2019-03-28 18:12:03 -0400

    fix migration

--- db/migrate/20190327194521_create_add_digest.rb
+++ db/migrate/20190327194521_create_add_digest.rb
@@ -1,7 +1,6 @@
 class CreateAddDigest < ActiveRecord::Migration[5.2]
   def change
     create_table :add_digests do |t|
-      add_column :users, :password_digest, :string, null: false 
     end
   end
 end
