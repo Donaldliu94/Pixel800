e38fdb67a92473dcfb5bb34e0c4c55ea52da0c20
Donaldliu94 <Donaldliu21194@gmail.com>
2019-09-13 19:30:24 -0400

    Modified comments css

--- frontend/components/comment/comment_item.jsx
+++ frontend/components/comment/comment_item.jsx
@@ -7,24 +7,27 @@ const CommentItem = ({comment}) => {
    
 
     return(
-        <div className="comment-entry">
+        <>
 
             <div className="comment-box">
-                <div className="comment-username">
-                    {comment.commentUsername}
+                <div className="comment-first-box">
+                    <div className="comment-username">
+                        {comment.commentUsername}
+                    </div>
+
+                    <div className="comment-date">
+                        {["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"][comment.created_at.slice(5, 7) - 1] + " " + comment.created_at.slice(8, 10) + ", " + comment.created_at.slice(0, 4)}
+                    </div>
                 </div>
 
-                <div className="comment-date">
-                    {["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"][comment.created_at.slice(5, 7) - 1] + " " + comment.created_at.slice(8, 10) + ", " + comment.created_at.slice(0, 4)}
-                </div>
 
+                <div className="comment-body">
+                    {comment.body} 
+                </div>
             </div>
 
 
-            {/* <div className="comment-body">
-                {comment.body} 
-            </div> */}
-        </div>
+        </>
     );
 
 
